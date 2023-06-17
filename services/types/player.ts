c9c630dd0dd324987b160f650d224d9099d82863
SampleSpaceDev <sam@samplespace.dev>
2023-06-17 18:14:10 +0100

    Convert Player to a class, and add ranks/social media

--- services/types/player.ts
+++ services/types/player.ts
@@ -1,14 +1,29 @@
 import { Bedwars } from "./bedwars";
 
-export interface PlayerResponse {
+export class PlayerResponse {
     player: Player;
 }
 
-interface Player {
+export class Player {
     displayname: string;
     uuid: string;
     stats: Stats;
     achievements: Achievements;
+
+    prefix?: string;
+    rank: string;
+    monthlyPackageRank: string;
+    newPackageRank: string;
+    packageRank: string;
+    monthlyRankColor: string;
+
+    socialMedia?: SocialMedia;
+}
+
+interface SocialMedia {
+    links: {
+        DISCORD?: string;
+    };
 }
 
 interface Achievements {
