13b6d20ed3f66e6f921e69e4e2c3310fd09fd627
SampleSpaceDev <sam@samplespace.dev>
2023-06-17 18:16:19 +0100

    Add autocomplete and expose properties

--- index.ts
+++ index.ts
@@ -12,7 +12,8 @@ import {
 import logger from "./util/logging";
 import { registerCommands, commands } from "./util/slashCommands";
 import * as config from "./config.json";
-import {registerFonts} from "./assets";
+import { registerFonts } from "./assets";
+import fs from "fs";
 
 const rest = new REST({ version: '10' }).setToken(config.token);
 
@@ -32,6 +33,9 @@ const client = new Client({ rest, gateway });
 export const webhooks: WebhooksAPI = new WebhooksAPI(rest);
 export const interactions: InteractionsAPI = new InteractionsAPI(rest, webhooks);
 
+const packageJson = fs.readFileSync('package.json', 'utf8');
+export const properties = JSON.parse(packageJson);
+
 client.once(GatewayDispatchEvents.Ready, async () => {
     logger.info("Client online.");
 
@@ -39,22 +43,25 @@ client.once(GatewayDispatchEvents.Ready, async () => {
 });
 
 client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: rawInteraction, api }) => {
-	if (rawInteraction.type !== InteractionType.ApplicationCommand) {
-		return;
-	}
-
     let interaction = rawInteraction as APIChatInputApplicationCommandInteraction;
-    let execute = commands.get(interaction.data.id);
-    
-    if (!execute) {
+    let command = commands.get(interaction.data.id);
+
+    if (!command) {
         return;
     }
 
     try {
-        await execute(interaction);
+        if (rawInteraction.type === InteractionType.ApplicationCommandAutocomplete) {
+            await command.autocomplete(interaction);
+        } else if (rawInteraction.type === InteractionType.ApplicationCommand) {
+            await command.execute(interaction);
+        }
     } catch (error) {
         logger.error(error);
+        console.error(error);
     }
 });
 
-gateway.connect();
+gateway.connect().then(r => {
+    logger.info("Gateway connected.");
+});
