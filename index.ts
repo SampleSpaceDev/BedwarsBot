62be494dba0c0209cdfae8bf42e8a3057d2e9d1c
SampleSpaceDev <sam@samplespace.dev>
2023-06-18 19:36:08 +0100

    Add heartbeats and online logging

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import {
     GatewayIntentBits,
     InteractionType,
     Client,
-    ApplicationCommandsAPI, InteractionsAPI, WebhooksAPI,
+    InteractionsAPI, WebhooksAPI,
     APIChatInputApplicationCommandInteraction
 } from "@discordjs/core";
 
@@ -14,6 +14,10 @@ import { registerCommands, commands } from "./util/slashCommands";
 import * as config from "./config.json";
 import { registerFonts } from "./assets";
 import fs from "fs";
+import axios from "axios";
+
+const heartbeatUrl = "https://status.samplespace.dev/api/push/6RqKRC8aZD?status=up&msg=OK&ping=";
+const statusWebhook = "";
 
 const rest = new REST({ version: '10' }).setToken(config.token);
 
@@ -25,10 +29,10 @@ const gateway = new WebSocketManager({
 
 const client = new Client({ rest, gateway });
 
-(async function slashCommands() {
-    const slashCommandApi = new ApplicationCommandsAPI(rest);
-    await registerCommands(slashCommandApi);
-})();
+// (async function slashCommands() {
+//     const slashCommandApi = new ApplicationCommandsAPI(rest);
+//
+// })();
 
 export const webhooks: WebhooksAPI = new WebhooksAPI(rest);
 export const interactions: InteractionsAPI = new InteractionsAPI(rest, webhooks);
@@ -37,12 +41,36 @@ const packageJson = fs.readFileSync('package.json', 'utf8');
 export const properties = JSON.parse(packageJson);
 
 client.once(GatewayDispatchEvents.Ready, async () => {
-    logger.info("Client online.");
+    logger.info("[CLIENT] Client online.");
 
+    const commit = extractCommitInfo();
+    if (commit) {
+        await axios.post(statusWebhook, {
+            content: {
+                "content": null,
+                "embeds": [
+                    {
+                        "title": "✅ Mango is now online.",
+                        "color": 4156010,
+                        "fields": [
+                            {
+                                "name": "Version",
+                                "value": `"[\`${commit.shortHash}\`](https://github.com/SampleSpaceDev/Mango/commit/${commit.hash}) - \`${commit.message}\`"`,
+                                "inline": true
+                            }
+                        ]
+                    }
+                ],
+                "attachments": []
+            }
+        });
+    }
+
+    await registerCommands(client.api.applicationCommands);
     await registerFonts();
 });
 
-client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: rawInteraction, api }) => {
+client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: rawInteraction}) => {
     let interaction = rawInteraction as APIChatInputApplicationCommandInteraction;
     let command = commands.get(interaction.data.id);
 
@@ -62,6 +90,32 @@ client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: rawInteraction
     }
 });
 
-gateway.connect().then(() => {
-    logger.info("Gateway connected.");
+const extractCommitInfo = () => {
+    if (fs.existsSync("./commit.txt") === false) {
+        return undefined;
+    }
+
+    const fileContent = fs.readFileSync("./commit.txt", 'utf-8');
+    const [commitHash, commitMessage] = fileContent.trim().split('\n');
+    const shortCommitHash = commitHash.substring(0, 7);
+
+    return {
+        hash: commitHash,
+        shortHash: shortCommitHash,
+        message: commitMessage,
+    };
+}
+
+const sendHeartbeat = async () => {
+    let response = await fetch(heartbeatUrl);
+    if (response.status !== 200) {
+        logger.error("[MONITOR] Failed to send heartbeat.");
+    }
+}
+
+gateway.connect().then(async () => {
+    logger.info("[CLIENT] Gateway connected.");
+
+    await sendHeartbeat();
+    setInterval(sendHeartbeat, 1000 * 60);
 });
