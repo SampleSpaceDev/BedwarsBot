82583e1556e36caeb9e0ff025a972aebaeac544b
SampleSpaceDev <sam@samplespace.dev>
2023-06-19 18:05:53 +0100

    Remove methods that should not exist and rework ratio

--- util/index.ts
+++ util/index.ts
@@ -1,28 +1,8 @@
-import { join } from "node:path";
-import { existsSync } from "node:fs";
 import { mongo } from "../services";
 import { LinkedPlayer } from "../services/types";
 import { Duration } from "moment";
 
-const PATH = "../../assets";
-const PRIVATE_PATH = join(PATH, "private");
-
-const hasPrivateAssets = existsSync(join(PRIVATE_PATH, "package.json"));
-
-const checkAsset = (file: string) =>
-  hasPrivateAssets && existsSync(join(PRIVATE_PATH, file)) ? "private" : "public";
-
-export type RemoveMethods<T> = Pick<
-  T,
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  { [Key in keyof T]: T[Key] extends Function ? never : Key }[keyof T]
->;
-
-export const noop = <T>() => null as unknown as T;
-
-export const ratio = (a: number, b: number): string => (b === 0 ? "∞" : (a / b).toFixed(2));
-
-export const getAssetPath = (path: string) => join(PATH, checkAsset(path), path);
+export const ratio = (a: number, b: number): string => (a === 0 && b === 0 ? "∞" : (a / Math.max(1, b)).toFixed(2));
 
 export const stripColor = (string: string) => string.replace(/<\/?[^>]+(>|$)|<#[^>]+(>|$)/g, "");
 
